c82b9b258bc06c583e45dac0965e9dc4c0dedb78
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-11-03 02:52:56 +0900

    Initial commit

---
+++ lib/kramdown/denden.rb
@@ -0,0 +1,7 @@
+require "kramdown/denden/version"
+
+module Kramdown
+  module Denden
+    # Your code goes here...
+  end
+end
---
+++ lib/kramdown/denden/version.rb
@@ -0,0 +1,5 @@
+module Kramdown
+  module Denden
+    VERSION = "0.0.1"
+  end
+end
