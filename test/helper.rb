fb93f53a4588fca3b0d3994ba87908f0e0f81d33
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-11-10 06:20:29 +0900

    Start SimpleCov before load lib files

--- test/helper.rb
+++ test/helper.rb
@@ -1,9 +1,9 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/test|vendor/'
+end
 require 'test-unit'
 require 'test/unit/notify'
 require 'kramdown'
 require 'kramdown/denden'
 require 'pathname'
-require 'simplecov'
-SimpleCov.start do
-  add_filter '/test|vendor/'
-end
