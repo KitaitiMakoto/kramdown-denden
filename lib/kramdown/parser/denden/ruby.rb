f49a26b6dd12b3972dcc70669137e8f15a7a72c6
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-11-10 07:43:29 +0900

    [REFACTORING]Extract method from #parse_ruby

--- lib/kramdown/parser/denden/ruby.rb
+++ lib/kramdown/parser/denden/ruby.rb
@@ -11,22 +11,24 @@ module Kramdown
         @src.pos += (rb.bytesize + 2) # +2 for "{" and "|"
         if rb.length == rts.length
           rb.each_char.with_index do |char, index|
-            ruby.children << Element.new(:text, char)
-            rt = Element.new(:rt)
-            parse_spans rt, /\||}/
-            @src.pos += 1 # +1 for "|" or "}"
-            ruby.children << rt
+            append_ruby_children ruby, char, /\||}/
           end
         else
-          ruby.children << Element.new(:text, rb)
-          rt = Element.new(:rt)
-          parse_spans rt, /}/
-          @src.pos += 1 # +1 for "}"
-          ruby.children << rt
+          append_ruby_children ruby, rb, /}/
         end
         @tree.children << ruby
       end
       Kramdown.define_parser :ruby, RUBY_START, '{'
+
+      private
+
+      def append_ruby_children(ruby, rb, stop_re)
+        ruby.children << Element.new(:text, rb)
+        rt = Element.new(:rt)
+        parse_spans rt, stop_re
+        @src.pos += 1 # +1 for "|" or "}"
+        ruby.children << rt
+      end
     end
   end
 end
